6b8bb6a192f8b13df06b07f17235d367468ca498
Kasturi Mitra <kasturi.mitra@Kasturis-MBP.lan>
2021-11-22 16:03:38 +0000

    added update todo api fixed

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -12,7 +12,7 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-  console.log(err);
+
   res.status(400).send({
 
     errors: [{ message: 'Something went wrong' }]
