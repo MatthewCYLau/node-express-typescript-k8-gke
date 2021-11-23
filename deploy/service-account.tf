2653a2fb9b5fbfc346e2eb6a16183fa9611b2eac
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-23 22:03:35 +0800

    feat: added service account for github ations

---
+++ deploy/service-account.tf
@@ -0,0 +1,22 @@
+resource "google_service_account" "github-actions" {
+  account_id   = "github-actions-service-account"
+  display_name = "Service account for Github Actions"
+}
+
+resource "google_project_iam_binding" "deploy" {
+  project = var.project
+  role    = "roles/container.serviceAgent"
+
+  members = [
+    "serviceAccount:${google_service_account.github-actions.email}"
+  ]
+}
+
+resource "google_project_iam_binding" "build" {
+  project = var.project
+  role    = "roles/cloudbuild.serviceAgent"
+
+  members = [
+    "serviceAccount:${google_service_account.github-actions.email}"
+  ]
+}
