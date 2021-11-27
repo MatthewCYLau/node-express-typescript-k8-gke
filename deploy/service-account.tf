1f61ca6ddb4e4a2f6d90390ad1740f9b17545579
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-27 20:59:35 +0800

    chore: removed un-used tf code for service account

--- deploy/service-account.tf
+++ deploy/service-account.tf
@@ -14,38 +14,3 @@ resource "google_project_iam_member" "build_agent" {
   role    = "roles/cloudbuild.serviceAgent"
   member  = "serviceAccount:${google_service_account.github-actions.email}"
 }
-
-# data "google_iam_policy" "github" {
-#   binding {
-#     role = "roles/container.serviceAgent"
-#     members = [
-#       "serviceAccount:${google_service_account.github-actions.email}"
-#     ]
-#   }
-#   binding {
-#     role = "roles/cloudbuild.serviceAgent"
-#     members = [
-#       "serviceAccount:${google_service_account.github-actions.email}"
-#     ]
-#   }
-# }
-
-/*
-resource "google_project_iam_binding" "deploy" {
-  project = var.project
-  role    = "roles/container.serviceAgent"
-
-  members = [
-    "serviceAccount:${google_service_account.github-actions.email}"
-  ]
-}
-
-resource "google_project_iam_binding" "build" {
-  project = var.project
-  role    = "roles/cloudbuild.serviceAgent"
-
-  members = [
-    "serviceAccount:${google_service_account.github-actions.email}"
-  ]
-}
-*/
