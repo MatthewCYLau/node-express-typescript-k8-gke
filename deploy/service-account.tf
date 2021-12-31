65c7b6298ffeac73e107d0a7e5f167d09facb547
Matthew Lau <lau.cy.matthew@gmail.com>
2021-12-31 16:23:01 +0000

    fix: updated gcp sa name

--- deploy/service-account.tf
+++ deploy/service-account.tf
@@ -1,6 +1,6 @@
 resource "google_service_account" "github-actions" {
-  account_id   = "github-actions-service-account"
-  display_name = "Service account for Github Actions"
+  account_id   = "github-actions-sa-node-gke"
+  display_name = "Service account for Node GKE project Github Action"
 }
 
 resource "google_project_iam_member" "gke_agent" {
